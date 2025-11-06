fabf68edd6935aae5d2d8ec1367e2159cbbbe726
Uche Nebed <nebeduch@gmail.com>
2025-11-06 00:19:24 -0500

    update nodepool and application

--- gcp/projects/personal-projects/k8s.tf
+++ gcp/projects/personal-projects/k8s.tf
@@ -25,33 +25,6 @@ resource "google_container_cluster" "sandbox" {
 
 }
 
-
-resource "google_container_node_pool" "workload" {
-  name_prefix = "workload-"
-  cluster    = google_container_cluster.sandbox.id
-  node_count = 2
-
-  management {
-    auto_repair = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "e2-small"
-    disk_size_gb = 20
-    disk_type = "pd-balanced"
-    image_type = "UBUNTU_CONTAINERD"
-    spot = true
-    resource_labels  = {
-       "goog-gke-node-pool-provisioning-model" = "spot"
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [ node_config[0].kubelet_config ]
-  }
-}
-
 resource "google_container_node_pool" "bitcoin" {
   name_prefix = "bitcoin-"
   cluster    = google_container_cluster.sandbox.id
@@ -63,8 +36,8 @@ resource "google_container_node_pool" "bitcoin" {
   }
 
   node_config {
-    machine_type = "c2d-standard-2"
-    disk_size_gb = 20
+    machine_type = "t2d-standard-4"
+    disk_size_gb = 80
     disk_type = "pd-balanced"
     image_type = "UBUNTU_CONTAINERD"
     spot = true
